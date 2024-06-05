d839b986ee2a564954992616f24294624ca19875
ohhawjenog <tjdtnr0428@naver.com>
2024-06-05 17:00:46 +0900

    0605-09
    Sensor 1, 2

---
+++ jh/Assets/Sensor.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sensor : MonoBehaviour
+{
+    public bool isObjectDetected = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Object"))
+        {
+            isObjectDetected = true;
+
+            if (this.gameObject.layer == LayerMask.NameToLayer("Destination"))
+            {
+                print(this.gameObject.name);
+            }
+        }
+    }
+}
