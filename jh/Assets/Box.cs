164c0ad8dcc676788f9e02de1bd819af7a42a339
ohhawjenog <tjdtnr0428@naver.com>
2024-06-17 16:36:09 +0900

    0617-02

--- jh/Assets/Box.cs
+++ jh/Assets/Box.cs
@@ -10,11 +10,16 @@ using UnityEngine.UI;
 public class Box : MonoBehaviour
 {
     public float speed = 1.0f;
-    public Sensor Sensor1;
-    public Sensor Sensor2;
+    public Sensor sensor1;
+    public Sensor sensor2;
     public Sensor sensor3;
     public Sensor sensor4;
-    public Sensor Sensor5;
+    public Sensor sensor5;
+    public DriveMotor xTransfer;
+    public DriveMotor yTransfer;
+    public DriveMotor zTransfer;
+    public bool boxADetected;
+    public bool boxBDetected;
     public Transform cylinder2;
     public float restartDistance = 1.0f; // 목적지에 도달하였다고 판단하는 거리
     private bool resultPrinted = false;
@@ -49,38 +54,48 @@ public class Box : MonoBehaviour
         {
             Vector3 front = new Vector3(1, 0, 0);
             transform.Translate(front * speed * Time.deltaTime);
-        }
 
-        if (Sensor1.isObjectDetected)
-        {
-            isStopped = true;
-            speed = 0;
-        }
-        if (Sensor2.isObjectDetected)
-        {
-            StartCoroutine(RestartAfterDelay(3.0f)); // 3초 후에 다시 시작
-        }
-        if (Sensor5.isObjectDetected)
-        {
-            isStopped = true;
-            speed = 0;
-        }
-        if (Sensor2.isObjectDetected)
-        {
-            if (!resultPrinted)
+            if (sensor1.isObjectDetected)
             {
-                if (sensor3.isSizeDetected && sensor4.isSizeDetected)
+                isStopped = true;
+                speed = 0;
+            }
+
+            if (sensor2.isObjectDetected)
+            {
+                StartCoroutine(RestartAfterDelay(3.0f)); // 3초 후에 다시 시작
+                if (!resultPrinted)
                 {
-                    Debug.Log("Box B");
-                    resultPrinted = true;
-                }
-                else if (!sensor3.isSizeDetected && sensor4.isSizeDetected)
-                {
-                    Debug.Log("Box A");
-                    resultPrinted = true;
+                    if (sensor3.isSizeDetected)
+                    {
+                        Debug.Log("Box A");
+                        xTransfer.boxACount++;
+                        yTransfer.boxACount++;
+                        zTransfer.boxACount++;
+                        boxADetected = true;
+                        boxBDetected = false;
+                        resultPrinted = true;
+                    }
+                    else if (sensor4.isSizeDetected)
+                    {
+                        Debug.Log("Box B");
+                        xTransfer.boxBCount++;
+                        yTransfer.boxBCount++;
+                        zTransfer.boxBCount++;
+                        boxADetected = false;
+                        boxBDetected = true;
+                        resultPrinted = true;
+                    }
                 }
             }
+            if (sensor5.isObjectDetected)
+            {
+                isStopped = true;
+                speed = 0;
+            }
         }
+
+        
     }
 
     private IEnumerator RestartAfterDelay(float delay)
--- jh/Assets/Lamp.cs
+++ jh/Assets/Lamp.cs
@@ -1,48 +1,24 @@
 using MPS;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Lamp : MonoBehaviour
 {
-    public string forwardName;
-    public string backwardName;
-    public int[] plcInputValues; // 편솔의 경우 입력 1개, 양솔의 경우 입력 2개
-    public Image forwardButtonImg;
-    public Image backwardButtonImg;
+    public Transform lampName;
 
-    // Start is called before the first frame update
-    void Start()
+    public int plcInputValue; 
+
+    private void Update()
     {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-        if (MxComponent.instance.connection == MxComponent.Connection.Connected)
+        if (plcInputValue > 0)
         {
-            // 스페이스바를 눌렀는지 확인
-            if (plcInputValues[0] > 0)
-            {
-                forwardButtonImg.color = Color.white;
-                backwardButtonImg.color = Color.green;
-            }
-            else
-            {
-                forwardButtonImg.color = Color.green;
-                backwardButtonImg.color = Color.white;
-            }
-
+            lampName.GetComponent<MeshRenderer>().material.color = Color.red;
         }
         else
-        {
-            //forwardButtonImg.color = Color.white;
-            //forwardButtonImg.color = Color.white;
-        }
-
-        
+            lampName.GetComponent<MeshRenderer>().material.color = Color.white;
     }
-
 }
+     
